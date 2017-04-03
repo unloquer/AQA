a1bba537e54112c13b8f23bc2934cda4b54ad2e5
brolin <brolin108@gmail.com>
2017-04-03 10:32:21 -0500

    Hace varios experimentos para el manejo del GPS

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,4 +1,4 @@
-#include <TinyGPS.h>
+#include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 #include <FS.h>
 #include <Wire.h>
@@ -46,18 +46,12 @@ int samples = 100;    // with delay of 100ms in between
   GPS Tx  --> ESP gpio12 (softserial Rx)
  */
 
-TinyGPS gps;
+TinyGPSPlus gps;
 FSInfo fs_info;
 static const int RXPin = 12, TXPin = 7;
 static const uint32_t GPSBaud = 9600;
 
 SoftwareSerial ss(RXPin, TXPin);
-
-static void smartdelay(unsigned long ms);
-static void print_float(float val, float invalid, int len, int prec);
-static void print_int(unsigned long val, unsigned long invalid, int len);
-static void print_date(TinyGPS &gps);
-static void print_str(const char *str, int len);
 
 
 //sensor de humedad
@@ -139,11 +133,20 @@ void setup()
   ss.begin(GPSBaud);
   SPIFFS.begin();
   pinMode(ledPower,OUTPUT);
-  Serial.println("Getting single-ended readings from AIN0..3");
-  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
   ads.begin();
   multiplier = ads.voltsPerBit()*1000.0F;           // Gets the millivolts per bit
   dht.begin();
+}
+
+static void smartdelay(unsigned long ms)
+{
+  unsigned long start = millis();
+  do
+    {
+      while (ss.available())
+        gps.encode(ss.read());
+    } while (millis() - start < ms);
+
 }
 
 void fs_write_data() {
@@ -170,149 +173,142 @@ void fs_write_data() {
   Serial.println(fs_total_space());
 }
 
-bool gps_data_available() {
-  while (ss.available())
+void displayInfo()
+{
+  Serial.print(F("Location: "));
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if(gps.altitude.isValid()) {
+    Serial.print(gps.altitude.meters());
+  }
+  else
+  {
+      Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if(gps.course.isValid()) {
+    Serial.print(gps.course.deg());
+  }
+  else
     {
-      char c = ss.read();
-      //Serial.write(c); // uncomment this line if you want to see the GPS data flowing
-      if (gps.encode(c)) // Did a new valid sentence come in?
-        return true;
+      Serial.print(F("INVALID"));
+    }
+
+  Serial.print(F(" "));
+  if(gps.course.isValid()) {
+    Serial.print(gps.speed.kmph());
+  }
+  else
+    {
+      Serial.print(F("INVALID"));
+    }
+
+  Serial.println();
+}
+
+bool gps_data_available() {
+  // This sketch displays information every time a new sentence is correctly encoded.
+  while (ss.available() > 0)
+    Serial.println("ENTRA");
+    if (gps.encode(ss.read()))
+      displayInfo();
+
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+    {
+      Serial.println(F("No GPS detected: check wiring."));
+      while(true);
     }
 }
 
-void gps_get_valid_position() {
-  //gps.f_get_position(&flat, &flon, &age);
-}
+// void gps_get_valid_position() {
+//   //gps.f_get_position(&flat, &flon, &age);
+// }
 
 void loop()
 {
-  float flat, flon;
-  unsigned long age, date, time, chars = 0;
-  unsigned short sentences = 0, failed = 0;
-  static const double LONDON_LAT = 51.508131, LONDON_LON = -0.128002;
+  //GPS
+  // This sketch displays information every time a new sentence is correctly encoded.
+  ESP.wdtDisable();
 
-  Serial.println("Sats HDOP Latitude  Longitude  Fix  Date       Time     Date Alt    Course Speed Card  Distance Course Card  Chars Sentences Checksum");
-  Serial.println("          (deg)     (deg)      Age                      Age  (m)    --- from GPS ----  ---- to London  ----  RX    RX        Fail");
-  Serial.println("-------------------------------------------------------------------------------------------------------------------------------------");
-  print_int(gps.satellites(), TinyGPS::GPS_INVALID_SATELLITES, 5);
-  print_int(gps.hdop(), TinyGPS::GPS_INVALID_HDOP, 5);
-  gps.f_get_position(&flat, &flon, &age);
-  print_float(flat, TinyGPS::GPS_INVALID_F_ANGLE, 10, 6);
-  print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
-  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-  print_date(gps);
-  print_float(gps.f_altitude(), TinyGPS::GPS_INVALID_F_ALTITUDE, 7, 2);
-  print_float(gps.f_course(), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
-  print_float(gps.f_speed_kmph(), TinyGPS::GPS_INVALID_F_SPEED, 6, 2);
-  print_str(gps.f_course() == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(gps.f_course()), 6);
-  print_int(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0xFFFFFFFF : (unsigned long)TinyGPS::distance_between(flat, flon, LONDON_LAT, LONDON_LON) / 1000, 0xFFFFFFFF, 9);
-  print_float(flat == TinyGPS::GPS_INVALID_F_ANGLE ? TinyGPS::GPS_INVALID_F_ANGLE : TinyGPS::course_to(flat, flon, LONDON_LAT, LONDON_LON), TinyGPS::GPS_INVALID_F_ANGLE, 7, 2);
-  print_str(flat == TinyGPS::GPS_INVALID_F_ANGLE ? "*** " : TinyGPS::cardinal(TinyGPS::course_to(flat, flon, LONDON_LAT, LONDON_LON)), 6);
+  displayInfo();
+  smartdelay(500);
+  // while (ss.available() > 0) if (gps.encode(ss.read())) displayInfo();
 
-  gps.stats(&chars, &sentences, &failed);
-  print_int(chars, 0xFFFFFFFF, 6);
-  print_int(sentences, 0xFFFFFFFF, 10);
-  print_int(failed, 0xFFFFFFFF, 9);
-  Serial.println();
+  // if (millis() > 5000 && gps.charsProcessed() < 10)
+  //   {
+  //     Serial.println(F("No GPS detected: check wiring."));
+  //     while(true);
+  //   }
+    // READ SHARP SENSOR
+  // dust_voltage = 0;
+  // for (int i=0;i<samples;i++) {
+  //   digitalWrite(ledPower,LOW); // power on the LED
+  //   delayMicroseconds(samplingTime);
+  //   voMeasured = 1000.0F*ads.readADC_SingleEnded_V(0); // read the dust voltage
+  //   delayMicroseconds(deltaTime);
+  //   digitalWrite(ledPower,HIGH); // turn the LED off
+  //   delayMicroseconds(sleepTime);
+  //   dust_voltage += voMeasured ;  // voltage 0-5V
+  //   delay(100);
+  // }
+  // dust_voltage = dust_voltage / samples;
 
-  // READ SHARP SENSOR
-  dust_voltage = 0;
-  for (int i=0;i<samples;i++) {
-    digitalWrite(ledPower,LOW); // power on the LED
-    delayMicroseconds(samplingTime);
-    voMeasured = 1000.0F*ads.readADC_SingleEnded_V(0); // read the dust voltage
-    delayMicroseconds(deltaTime);
-    digitalWrite(ledPower,HIGH); // turn the LED off
-    delayMicroseconds(sleepTime);
-    dust_voltage += voMeasured ;  // voltage 0-5V
-    delay(100);
-  }
-  dust_voltage = dust_voltage / samples;
+  // // CALCULATE SHARP RESULTS
+  // concSHARP = 0.172*dust_voltage-0.1;        // density microgram/m3
+  // if (concSHARP > 500) concSHARP = 500;
 
-  // CALCULATE SHARP RESULTS
-  concSHARP = 0.172*dust_voltage-0.1;        // density microgram/m3
-  if (concSHARP > 500) concSHARP = 500;
+  // // PRINT SHARP DATA TO SERIAL
+  // Serial.print(dust_voltage);
+  // Serial.print("\t\t");
+  // Serial.println(concSHARP);
 
-  // PRINT SHARP DATA TO SERIAL
-  Serial.print(dust_voltage);
-  Serial.print("\t\t");
-  Serial.println(concSHARP);
-
-  // Read humidity
-  float h = get_humidity();
-  // Read temperature as Celsius
-  float t = get_termperature();
-}
-
-static void smartdelay(unsigned long ms)
-{
-  unsigned long start = millis();
-  do 
-  {
-    while (ss.available())
-      gps.encode(ss.read());
-  } while (millis() - start < ms);
-}
-
-static void print_float(float val, float invalid, int len, int prec)
-{
-  if (val == invalid)
-  {
-    while (len-- > 1)
-      Serial.print('*');
-    Serial.print(' ');
-  }
-  else
-  {
-    Serial.print(val, prec);
-    int vi = abs((int)val);
-    int flen = prec + (val < 0.0 ? 2 : 1); // . and -
-    flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
-    for (int i=flen; i<len; ++i)
-      Serial.print(' ');
-  }
-  smartdelay(0);
-}
-
-static void print_int(unsigned long val, unsigned long invalid, int len)
-{
-  char sz[32];
-  if (val == invalid)
-    strcpy(sz, "*******");
-  else
-    sprintf(sz, "%ld", val);
-  sz[len] = 0;
-  for (int i=strlen(sz); i<len; ++i)
-    sz[i] = ' ';
-  if (len > 0) 
-    sz[len-1] = ' ';
-  Serial.print(sz);
-  smartdelay(0);
-}
-
-static void print_date(TinyGPS &gps)
-{
-  int year;
-  byte month, day, hour, minute, second, hundredths;
-  unsigned long age;
-  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
-  if (age == TinyGPS::GPS_INVALID_AGE)
-    Serial.print("********** ******** ");
-  else
-  {
-    char sz[32];
-    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
-        month, day, year, hour, minute, second);
-    Serial.print(sz);
-  }
-  print_int(age, TinyGPS::GPS_INVALID_AGE, 5);
-  smartdelay(0);
-}
-
-static void print_str(const char *str, int len)
-{
-  int slen = strlen(str);
-  for (int i=0; i<len; ++i)
-    Serial.print(i<slen ? str[i] : ' ');
-  smartdelay(0);
+  // // Read humidity
+  // float h = get_humidity();
+  // // Read temperature as Celsius
+  // float t = get_termperature();
 }
