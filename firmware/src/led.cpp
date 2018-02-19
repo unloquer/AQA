8c78c0e39387a345a8208128e0a19a17364db1b4
brolin <brolin108@gmail.com>
2018-02-18 23:10:33 -0500

    Corrige pines de los leds

--- firmware/src/led.cpp
+++ firmware/src/led.cpp
@@ -8,12 +8,13 @@
 
 // Pines 16 y 14 no sirven para la librería fastled
 #define NUM_LEDS 2
-#define DI 4//D3 //4  //0  // Si se usa la tarjeta wemos d1 mini se antepone la letra D a los pines
-#define CI 2//D2 //2 //4  // si se usa la roja de uber se unas los pines directamente
+#define DI D3 //4  //0  // Si se usa la tarjeta wemos d1 mini se antepone la letra D a los pines
+#define CI D2 //2 //4  // si se usa la roja de uber se unas los pines directamente
 CRGB leds[NUM_LEDS];
+#define COLOR_ORDER GRB
 
 void setupLeds() {
-  FastLED.addLeds<LPD8806, DI, CI>(leds, NUM_LEDS);
+  FastLED.addLeds<LPD8806, DI, CI, COLOR_ORDER>(leds, NUM_LEDS);
 }
 
 void ledParticulateQuality(PlantowerData data) {
@@ -23,10 +24,10 @@ void ledParticulateQuality(PlantowerData data) {
 
   CRGB alert = CRGB::Black;
 
-  if(pm25 < 13) alert = CRGB::Red; // CRGB::Green; // Alert.ok
-  if(pm25 >= 13 && pm25 < 35) alert = CRGB::OrangeRed; // Alert.notGood;
+  if(pm25 < 13) alert = CRGB::Green; // CRGB::Green; // Alert.ok
+  if(pm25 >= 13 && pm25 < 35) alert = CRGB::Orange; // Alert.notGood;
   if(pm25 >= 35 && pm25 < 55) alert = CRGB::Yellow; // Alert.bad;
-  if(pm25 >= 55 && pm25 < 75) alert = CRGB::Green; // CRGB::Red; // Alert.dangerous;
+  if(pm25 >= 55 && pm25 < 75) alert = CRGB::Red; // CRGB::Red; // Alert.dangerous;
   if(pm25 >= 75) alert = CRGB::Purple; // Alert.harmful;
 
   FastLED.setBrightness(millis() % 255);
@@ -37,6 +38,7 @@ void ledParticulateQuality(PlantowerData data) {
     delay(300);
   }
 }
+
   void ledParticulateQuality2(PlantowerData data) {
     int pm1 = data.pm1;
     int pm25 = data.pm25;
