9578a34e773d17f1b2e5d82fed11cca4c42420f8
son0p <fede2001@gmail.com>
2018-04-01 18:50:20 -0500

    agrega script para recrear datos de sensores, guardados en archivos

--- utils/mobileMeasuresGenerator.sh
+++ utils/mobileMeasuresGenerator.sh
@@ -26,9 +26,23 @@ bold=$(tput bold)
 while(true)
 do
     pm25=$((1 + RANDOM % 200));
-    lat=$(python -c "import random;print(random.uniform(6.150, 6.189))"); 
-    lng=$(python -c "import random;print(random.uniform(-75.50, -75.59))"); 
+    lat=$(python -c "import random;print(random.uniform(6.240, 6.249))"); 
+    lng=$(python -c "import random;print(random.uniform(-75.580, -75.585))"); 
     curl -i -XPOST 'http://localhost:8086/write?db=aqaTest' --data-binary "volkerX,id=volkerX pm25=$pm25,lat=$lat,lng=$lng";
+    sleep 1;
+    pm25=$((1 + RANDOM % 200));
+    lat=$(python -c "import random;print(random.uniform(6.260, 6.265))"); 
+    lng=$(python -c "import random;print(random.uniform(-75.570, -75.575))"); 
+    curl -i -XPOST 'http://localhost:8086/write?db=aqaTest' --data-binary "volkerX001,id=volkerX pm25=$pm25,lat=$lat,lng=$lng";
+    pm25=$((1 + RANDOM % 200));
+    lat=$(python -c "import random;print(random.uniform(6.250, 6.259))"); 
+    lng=$(python -c "import random;print(random.uniform(-75.580, -75.589))"); 
+    curl -i -XPOST 'http://localhost:8086/write?db=aqaTest' --data-binary "volkerX002,id=volkerX pm25=$pm25,lat=$lat,lng=$lng";
+    sleep 1;
+    pm25=$((1 + RANDOM % 200));
+    lat=$(python -c "import random;print(random.uniform(6.230, 6.235))"); 
+    lng=$(python -c "import random;print(random.uniform(-75.570, -75.575))"); 
+    curl -i -XPOST 'http://localhost:8086/write?db=aqaTest' --data-binary "volkerX003,id=volkerX pm25=$pm25,lat=$lat,lng=$lng";
     sleep 1;
 done
 exit 0
